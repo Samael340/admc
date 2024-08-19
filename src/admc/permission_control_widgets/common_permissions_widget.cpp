512eadba5c73ff28b5b1738cf8349176d53c7748
Semyon Knyazev <samael@altlinux.org>
2024-08-20 02:45:57 +0400

    Create common permissions widget
    Common permission functionality is removed from security tab sources
    and placed in the CommonRightsWidget class.

---
+++ src/admc/permission_control_widgets/common_permissions_widget.cpp
@@ -0,0 +1,111 @@
+/*
+ * ADMC - AD Management Center
+ *
+ * Copyright (C) 2020-2024 BaseALT Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common_permissions_widget.h"
+
+#include "adldap.h"
+#include "globals.h"
+#include "settings.h"
+#include "utils.h"
+
+#include "samba/ndr_security.h"
+
+#include <QSortFilterProxyModel>
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QTreeView>
+#include <QVBoxLayout>
+#include <QDebug>
+
+class CommonRightsSortModel final : public QSortFilterProxyModel {
+public:
+    using QSortFilterProxyModel::QSortFilterProxyModel;
+
+    bool lessThan(const QModelIndex &source_left, const QModelIndex &source_right) const override {
+        SecurityRight left = source_left.data(RightsItemRole_SecurityRight).
+                value<SecurityRight>();
+        SecurityRight right = source_right.data(RightsItemRole_SecurityRight).
+                value<SecurityRight>();
+        const uint32_t access_mask_left = left.access_mask;
+        const uint32_t access_mask_right = right.access_mask;
+
+        // Generic among generic are in pre-defined order
+        const int common_index_left = common_rights_list.indexOf(access_mask_left);
+        const int common_index_right = common_rights_list.indexOf(access_mask_right);
+
+        return (common_index_left < common_index_right);
+    }
+};
+
+CommonPermissionsWidget::CommonPermissionsWidget(QWidget *parent) : PermissionsWidget(parent) {
+    v_layout->addWidget(rights_view);
+
+    rights_sort_model = new CommonRightsSortModel(this);
+    rights_sort_model->setSourceModel(rights_model);
+
+    rights_view->setModel(rights_sort_model);
+    rights_view->setColumnWidth(PermissionColumn_Name, 400);
+
+    settings_restore_header_state(SETTING_common_permissions_header_state, rights_view->header());
+}
+
+CommonPermissionsWidget::~CommonPermissionsWidget() {
+    settings_save_header_state(SETTING_common_permissions_header_state, rights_view->header());
+}
+
+void CommonPermissionsWidget::init(const QStringList &target_classes, security_descriptor *sd_arg) {
+    sd = sd_arg;
+    target_class_list = target_classes;
+    rights_model->removeRows(0, rights_model->rowCount());
+
+    // Create items in rights model. These will not
+    // change until target object changes. Only the
+    // state of items changes during editing.
+    const QList<SecurityRight> right_list = ad_security_get_common_rights();
+    for (const SecurityRight &right : right_list) {
+        const QList<QStandardItem *> row = make_item_row(PermissionColumn_COUNT);
+
+        // TODO: for russian, probably do "read/write
+        // property - [property name]" to avoid having
+        // to do suffixes properties
+        const QString right_name = ad_security_get_right_name(g_adconfig, right, language);
+
+        row[PermissionColumn_Name]->setText(right_name);
+        row[PermissionColumn_Name]->setEditable(false);
+        row[PermissionColumn_Allowed]->setCheckable(true);
+        row[PermissionColumn_Allowed]->setEditable(false);
+        row[PermissionColumn_Denied]->setCheckable(true);
+        row[PermissionColumn_Denied]->setEditable(false);
+
+        QVariant right_data;
+        right_data.setValue(right);
+        row[0]->setData(right_data, RightsItemRole_SecurityRight);
+
+        rights_model->appendRow(row);
+    }
+
+    // NOTE: because rights model is dynamically filled
+    // when trustee switches, we have to make rights
+    // model read only again after it's reloaded
+    if (read_only) {
+        make_model_rights_read_only();
+    }
+
+    rights_sort_model->sort(0);
+}
